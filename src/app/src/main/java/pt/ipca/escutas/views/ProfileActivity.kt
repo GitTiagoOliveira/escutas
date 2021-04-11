73200fe5a02298480d8fec84d195589c1d3dc155
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-11 15:02:07 +0100

    Add initial views

---
+++ src/app/src/main/java/pt/ipca/escutas/views/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipca.escutas.views
+
+import android.os.Bundle
+import pt.ipca.escutas.R
+
+class ProfileActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
