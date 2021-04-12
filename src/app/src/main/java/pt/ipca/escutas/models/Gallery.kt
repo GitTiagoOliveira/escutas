3ddb81cfacfdbe8435cf354c35ccd5de8bb29260
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-12 11:51:08 +0100

    Add initial models

---
+++ src/app/src/main/java/pt/ipca/escutas/models/Gallery.kt
@@ -0,0 +1,9 @@
+package pt.ipca.escutas.models
+
+/**
+ * Defines the model of a gallery. Contains a collection of [Image] and related metadata.
+ *
+ */
+class Gallery {
+    // TODO
+}
---
+++ src/app/src/main/java/pt/ipca/escutas/models/User.kt
@@ -0,0 +1,9 @@
+package pt.ipca.escutas.models
+
+/**
+ * Defines the model of an application user.
+ *
+ */
+class User {
+    // TODO
+}
