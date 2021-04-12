2d28145bcb3ff2cfef20ad53f812b8cdd8eef18d
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-12 11:28:24 +0100

    Add initial storage service

---
+++ src/app/src/main/java/pt/ipca/escutas/services/FirebaseStorageService.kt
@@ -0,0 +1,9 @@
+package pt.ipca.escutas.services
+
+/**
+ * Defines a Firebase implementation of a [IStorageService].
+ *
+ */
+class FirebaseStorageService : IStorageService {
+    // TODO
+}
---
+++ src/app/src/main/java/pt/ipca/escutas/services/IStorageService.kt
@@ -0,0 +1,9 @@
+package pt.ipca.escutas.services
+
+/**
+ * Defines the contract of a storage service.
+ *
+ */
+interface IStorageService {
+    // TODO
+}
