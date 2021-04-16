adcd5fa634c71d7ae74efd90b28e7ca6cf15be6c
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-16 11:27:22 +0100

    Add behavior to storage service contract

--- src/app/src/main/java/pt/ipca/escutas/services/FirebaseStorageService.kt
+++ src/app/src/main/java/pt/ipca/escutas/services/FirebaseStorageService.kt
@@ -1,11 +1,60 @@
 package pt.ipca.escutas.services
 
+import com.google.firebase.storage.StorageReference
 import pt.ipca.escutas.services.contracts.IStorageService
+import java.io.FileInputStream
 
 /**
  * Defines a Firebase implementation of an [IStorageService].
  *
  */
 class FirebaseStorageService : IStorageService {
-    // TODO
+    /**
+     * Creates a file in the storage service through the specified [filePath] and [fileStream].
+     *
+     * @param filePath The destination file path in the storage service.
+     * @param fileStream The file input stream.
+     */
+    override fun createFile(filePath: String, fileStream: FileInputStream) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Reads a file in the storage service through the specified [filePath].
+     *
+     * @param filePath The file path in the storage service.
+     * @return The file byte sequence.
+     */
+    override fun readFile(filePath: String): Array<Byte> {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Reads a file storage reference in the storage service through the specified [filePath].
+     *
+     * @param filePath The file path in the storage service.
+     * @return The file reference in the storage service.
+     */
+    fun readReference(filePath: String): StorageReference {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Updates a file in the storage service through the specified [filePath] and [fileStream].
+     *
+     * @param filePath The file path in the storage service.
+     * @param fileStream The file input stream.
+     */
+    override fun updateFile(filePath: String, fileStream: FileInputStream) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Deletes a file in the storage service through the specified [filePath].
+     *
+     * @param filePath The file path in the storage service.
+     */
+    override fun deleteFile(filePath: String) {
+        TODO("Not yet implemented")
+    }
 }
