a478f35fb2e5fea8cdbb74c6ae44e0a40b689a3a
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-16 11:27:22 +0100

    Add behavior to storage service contract

--- src/app/src/main/java/pt/ipca/escutas/services/contracts/IStorageService.kt
+++ src/app/src/main/java/pt/ipca/escutas/services/contracts/IStorageService.kt
@@ -1,9 +1,40 @@
 package pt.ipca.escutas.services.contracts
 
+import java.io.FileInputStream
+
 /**
  * Defines the contract of a storage service.
  *
  */
 interface IStorageService {
-    // TODO
+    /**
+     * Creates a file in the storage service through the specified [filePath] and [fileStream].
+     *
+     * @param filePath The destination file path in the storage service.
+     * @param fileStream The file input stream.
+     */
+    fun createFile(filePath: String, fileStream: FileInputStream)
+
+    /**
+     * Reads a file in the storage service through the specified [filePath].
+     *
+     * @param filePath The file path in the storage service.
+     * @return The file byte sequence.
+     */
+    fun readFile(filePath: String): Array<Byte>
+
+    /**
+     * Updates a file in the storage service through the specified [filePath] and [fileStream].
+     *
+     * @param filePath The file path in the storage service.
+     * @param fileStream The file input stream.
+     */
+    fun updateFile(filePath: String, fileStream: FileInputStream)
+
+    /**
+     * Deletes a file in the storage service through the specified [filePath].
+     *
+     * @param filePath The file path in the storage service.
+     */
+    fun deleteFile(filePath: String)
 }
