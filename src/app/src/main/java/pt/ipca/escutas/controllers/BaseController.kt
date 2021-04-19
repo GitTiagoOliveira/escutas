621314bebd8101695264d8fd3713dd1ddaa17947
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-19 21:50:37 +0100

    Add base controller properties

--- src/app/src/main/java/pt/ipca/escutas/controllers/BaseController.kt
+++ src/app/src/main/java/pt/ipca/escutas/controllers/BaseController.kt
@@ -1,9 +1,32 @@
 package pt.ipca.escutas.controllers
 
+import android.content.res.Resources
+import pt.ipca.escutas.services.FirebaseAuthService
+import pt.ipca.escutas.services.FirebaseDatabaseService
+import pt.ipca.escutas.services.FirebaseStorageService
+
 /**
  * Defines the base controller. Further controllers should inherit from this base.
  *
  */
 open class BaseController {
-    // TODO: Add database and storage services through dependency injection.
+    /**
+     * The authentication service.
+     */
+    protected val auth: FirebaseAuthService = FirebaseAuthService()
+
+    /**
+     * The database service.
+     */
+    protected val database: FirebaseDatabaseService = FirebaseDatabaseService()
+
+    /**
+     * The storage service.
+     */
+    protected val storage: FirebaseStorageService = FirebaseStorageService()
+
+    /**
+     * The application resources accessor.
+     */
+    protected val resources: Resources = Resources.getSystem()
 }
