fcf9c491ee7529bb9b740de4feccb986801d8e6a
Tiago Oliveira <td.oliveira@campus.fct.unl.pt>
2021-04-20 11:22:52 +0100

    Sprint01 - Added about and registration behaviours (Login Activity)

--- src/app/src/main/java/pt/ipca/escutas/views/LoginActivity.kt
+++ src/app/src/main/java/pt/ipca/escutas/views/LoginActivity.kt
@@ -1,6 +1,8 @@
 package pt.ipca.escutas.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import pt.ipca.escutas.R
 
@@ -12,5 +14,19 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Sobre Click Action
+        var aboutTextView = findViewById<TextView>(R.id.sobre)
+        aboutTextView.setOnClickListener{
+            val intent = Intent(this@LoginActivity, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Registration Click Action
+        var registerTextView = findViewById<TextView>(R.id.register)
+        registerTextView.setOnClickListener{
+            val intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
