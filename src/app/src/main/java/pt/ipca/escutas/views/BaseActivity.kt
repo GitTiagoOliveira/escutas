554fa4b6ae42526cab7b0349cc8c46a8e829c206
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-24 23:22:43 +0100

    Add toolbar

--- src/app/src/main/java/pt/ipca/escutas/views/BaseActivity.kt
+++ src/app/src/main/java/pt/ipca/escutas/views/BaseActivity.kt
@@ -2,6 +2,7 @@ package pt.ipca.escutas.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import pt.ipca.escutas.R
 
@@ -11,20 +12,34 @@ import pt.ipca.escutas.R
  */
 open class BaseActivity : AppCompatActivity() {
     /**
+     * The toolbar.
+     */
+    protected lateinit var toolbar: Toolbar
+
+    /**
+     * The navigation menu.
+     */
+    protected lateinit var navigationMenu: BottomNavigationView
+
+    /**
      * The navigation menu item listener.
      */
     private val onNavigationMenuItemListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_newsfeed -> {
+                this.toolbar.setTitle(R.string.menu_bottom_navigation_news_feed)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_map -> {
+                this.toolbar.setTitle(R.string.menu_bottom_navigation_map)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_calendar -> {
+                this.toolbar.setTitle(R.string.menu_bottom_navigation_calendar)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_gallery -> {
+                this.toolbar.setTitle(R.string.menu_bottom_navigation_gallery)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -41,6 +56,10 @@ open class BaseActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_base)
 
-        val navigationMenu: BottomNavigationView = findViewById(R.id.navigation_menu)
+        this.toolbar = findViewById(R.id.toolbar)
+        this.navigationMenu = findViewById(R.id.navigation_menu)
+
+        setSupportActionBar(this.toolbar)
+        this.navigationMenu.setOnNavigationItemSelectedListener(onNavigationMenuItemListener)
     }
 }
