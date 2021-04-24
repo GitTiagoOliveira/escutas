678d2881b3d3a6be44747b1c8e71430417700227
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-04-24 21:38:02 +0100

    Add navigation menu

--- src/app/src/main/java/pt/ipca/escutas/views/BaseActivity.kt
+++ src/app/src/main/java/pt/ipca/escutas/views/BaseActivity.kt
@@ -2,6 +2,7 @@ package pt.ipca.escutas.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import pt.ipca.escutas.R
 
 /**
@@ -9,8 +10,37 @@ import pt.ipca.escutas.R
  *
  */
 open class BaseActivity : AppCompatActivity() {
+    /**
+     * The navigation menu item listener.
+     */
+    private val onNavigationMenuItemListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_newsfeed -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_map -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_calendar -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_gallery -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    /**
+     * Invoked when the activity is starting.
+     *
+     * @param savedInstanceState The saved instance state.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_base)
+
+        val navigationMenu: BottomNavigationView = findViewById(R.id.navigation_menu)
     }
 }
