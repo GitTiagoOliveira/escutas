e19b6c1d556846d168b2f2505dc244ecbd693732
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-05-06 23:33:31 +0100

    Add initial map view

--- src/app/src/main/java/pt/ipca/escutas/controllers/MapController.kt
+++ src/app/src/main/java/pt/ipca/escutas/controllers/MapController.kt
@@ -1,11 +1,29 @@
 package pt.ipca.escutas.controllers
 
+import pt.ipca.escutas.models.Location
 import pt.ipca.escutas.views.fragments.MapFragment
+import java.util.*
 
 /**
  * Defines the [MapFragment] controller.
  *
  */
 class MapController : BaseController() {
-    // TODO
+    /**
+     * Gets the stored locations.
+     *
+     * @return A list containing the stored locations.
+     */
+    fun getLocations(): List<Location>
+    {
+        // TODO: Invoke database service in order to fetch locations. Response is mocked for now.
+
+        return listOf(
+            Location(
+                UUID.randomUUID(),
+                "Sede do Agrupamento 1 - Sé de Braga",
+                41.5518896,
+                -8.4309099)
+        )
+    }
 }
--- src/app/src/main/java/pt/ipca/escutas/views/fragments/MapFragment.kt
+++ src/app/src/main/java/pt/ipca/escutas/views/fragments/MapFragment.kt
@@ -5,13 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import pt.ipca.escutas.R
+import pt.ipca.escutas.controllers.MapController
+import pt.ipca.escutas.models.Location
 
 /**
  * Defines the map fragment.
  *
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    /**
+     * The map controller.
+     */
+    private val mapController: MapController by lazy { MapController() }
+
     /**
      * Invoked when the fragment instantiates his view.
      *
@@ -20,8 +33,35 @@ class MapFragment : Fragment() {
      * @param savedInstanceState The saved instance state.
      * @return The fragment view.
      */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_map, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_map, container, false)
+        val map: SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        map.getMapAsync(this)
+
+        return view
+    }
+
+    /**
+     * Applies the desired behavior when the map is ready.
+     *
+     * @param map The map.
+     */
+    override fun onMapReady(map: GoogleMap?) {
+        map?.apply {
+            val locations: List<Location> = mapController.getLocations()
+
+            for (location in locations) {
+                val coords = LatLng(location.latitude, location.longitude)
+
+                addMarker(
+                    MarkerOptions()
+                        .position(coords)
+                        .title(location.name)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+            }
+        }
+    }
 
     companion object {
         /**
@@ -29,6 +69,6 @@ class MapFragment : Fragment() {
          *
          * @return A instance of the current fragment.
          */
-        fun getInstance(): GalleryFragment = GalleryFragment()
+        fun getInstance(): MapFragment = MapFragment()
     }
 }
