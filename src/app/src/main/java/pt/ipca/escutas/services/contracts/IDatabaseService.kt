a9140f600265d740f5bdd2906c00170e42425dd7
Tiago Oliveira <td.oliveira@campus.fct.unl.pt>
2021-05-17 19:08:29 +0100

    Sprint03 - Add DatabaseService with generic methods to be used by each controller.

--- src/app/src/main/java/pt/ipca/escutas/services/contracts/IDatabaseService.kt
+++ src/app/src/main/java/pt/ipca/escutas/services/contracts/IDatabaseService.kt
@@ -5,5 +5,67 @@ package pt.ipca.escutas.services.contracts
  *
  */
 interface IDatabaseService {
-    // TODO
+
+    /**
+     * Adds a new document [record] to a specific collection [model]
+     *
+     * @param model
+     * @param record
+     */
+    fun addRecord(model: String, record: Any)
+
+    /**
+     * Updates a record [record] associated to document [documentId] of a specific collection [model]
+     *
+     * @param model
+     * @param documentId
+     * @param record
+     */
+    fun updateRecord(model: String, documentId: String, record: Any)
+
+    /**
+     * Deletes a document [documentId] of a specific collection [model]
+     *
+     * @param model
+     * @param documentId
+     */
+    fun deleteRecord(model: String, documentId: String)
+
+    /**
+     * Retrieves all records of a specific collection [model]
+     *
+     * @param model
+     * @return
+     */
+    fun getAllRecords(model: String): HashMap<String, Any>
+
+    /**
+     * Retrieves all records that respect an equal filter based on [recordKey] and [recordValue] of a specific collection [model]
+     *
+     * @param model
+     * @param recordKey
+     * @param recordValue
+     * @return
+     */
+    fun getRecordWithEqualFilter(model: String, recordKey: String, recordValue: Any): HashMap<String, Any>
+
+    /**
+     * Retrieves all records that respect an greater than filter based on [recordKey] and [recordValue] of a specific collection [model]
+     *
+     * @param model
+     * @param recordKey
+     * @param recordValue
+     * @return
+     */
+    fun getRecordWithGreaterThanFilter(model: String, recordKey: String, recordValue: Any): HashMap<String, Any>
+
+    /**
+     * Retrieves all records that respect an lesser than filter based on [recordKey] and [recordValue] of a specific collection [model]
+     *
+     * @param model
+     * @param recordKey
+     * @param recordValue
+     * @return
+     */
+    fun getRecordWithLessThanFilter(model: String, recordKey: String, recordValue: Any): HashMap<String, Any>
 }
