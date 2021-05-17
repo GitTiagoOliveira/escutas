cbeea5f6ea3c4f036aa8820fe44ca0870c729015
Tiago Oliveira <td.oliveira@campus.fct.unl.pt>
2021-05-17 19:08:29 +0100

    Sprint03 - Add DatabaseService with generic methods to be used by each controller.

--- src/app/src/main/java/pt/ipca/escutas/services/FirebaseDatabaseService.kt
+++ src/app/src/main/java/pt/ipca/escutas/services/FirebaseDatabaseService.kt
@@ -1,5 +1,10 @@
 package pt.ipca.escutas.services
 
+import android.content.ContentValues
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.firestore.FirebaseFirestore
+import pt.ipca.escutas.resources.Strings
 import pt.ipca.escutas.services.contracts.IDatabaseService
 
 /**
@@ -7,5 +12,111 @@ import pt.ipca.escutas.services.contracts.IDatabaseService
  *
  */
 class FirebaseDatabaseService : IDatabaseService {
-    // TODO
+
+    private val storage: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    override fun addRecord(model: String, record: Any) {
+
+        val modelData = this.storage.collection(model)
+
+        modelData.add(record).addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_ADD, task.exception)
+            }
+        }
+    }
+
+    override fun updateRecord(model: String, documentId: String, record: Any) {
+
+        val modelData = this.storage.collection(model)
+
+        modelData.document(documentId).set(record).addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_UPDATE, task.exception)
+            }
+        }
+    }
+
+    override fun deleteRecord(model: String, documentId: String) {
+
+        val modelData = this.storage.collection(model)
+
+        modelData.document(documentId).delete().addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_REMOVE, task.exception)
+            }
+        }
+    }
+
+    override fun getAllRecords(model: String): HashMap<String, Any> {
+
+        val modelData = this.storage.collection(model)
+        val output = HashMap<String, Any>()
+
+        modelData.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                for (document in task.result!!) {
+                    output[document.id] = document.data
+                }
+            } else {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_GET, task.exception)
+            }
+        }
+
+        return output
+    }
+
+    override fun getRecordWithEqualFilter(model: String, recordKey: String, recordValue: Any): HashMap<String, Any> {
+
+        val modelData = this.storage.collection(model)
+        val output = HashMap<String, Any>()
+
+        modelData.whereEqualTo(recordKey, recordValue).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                for (document in task.result!!) {
+                    output[document.id] = document.data
+                }
+            } else {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_GET, task.exception)
+            }
+        }
+
+        return output
+    }
+
+    override fun getRecordWithGreaterThanFilter(model: String, recordKey: String, recordValue: Any): HashMap<String, Any> {
+
+        val modelData = this.storage.collection(model)
+        val output = HashMap<String, Any>()
+
+        modelData.whereGreaterThan(recordKey, recordValue).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                for (document in task.result!!) {
+                    output[document.id] = document.data
+                }
+            } else {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_GET, task.exception)
+            }
+        }
+
+        return output
+    }
+
+    override fun getRecordWithLessThanFilter(model: String, recordKey: String, recordValue: Any): HashMap<String, Any> {
+
+        val modelData = this.storage.collection(model)
+        val output = HashMap<String, Any>()
+
+        modelData.whereLessThan(recordKey, recordValue).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                for (document in task.result!!) {
+                    output[document.id] = document.data
+                }
+            } else {
+                Log.d(ContentValues.TAG, Strings.MSG_FAIL_DATABASE_GET, task.exception)
+            }
+        }
+
+        return output
+    }
 }
